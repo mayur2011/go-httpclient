169d367b5cf47785db796f08c01a0e0e56f8be29
mayur2011 <er.mayur2011@gmail.com>
2022-01-04 22:05:07 +0530

    Testing GO HTTP

--- gohttp/client_core_test.go
+++ gohttp/client_core_test.go
@@ -1,1 +1,25 @@
 package gohttp
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetRequestHeaders(t *testing.T) {
+	//Initialization
+	client := httpClient{}
+	commonHeaders := make(http.Header)
+	commonHeaders.Set("Content-Type", "application/json")
+	commonHeaders.Set("User-Agent", "cool-http-client")
+	client.Headers = commonHeaders
+
+	//Execution
+	requestHeaders := make(http.Header)
+	requestHeaders.Set("X-Request-Id", "ABC-123")
+	finalheaders := client.getRequestHeaders(requestHeaders)
+
+	//Validation
+	if len(finalheaders) != 3 {
+		t.Error("expecting 3 headers")
+	}
+}
